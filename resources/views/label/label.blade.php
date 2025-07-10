bade84d3830f07517e23308763792ed193d5a070
ahmadFairuuz <ahmadfruz@gmail.com>
2025-07-10 11:13:30 +0700

    update ui sidebar, label.create, label.edit

--- resources/views/label/label.blade.php
+++ resources/views/label/label.blade.php
@@ -1,17 +1,10 @@
 @extends('layout.main')
-
-{{-- SECTION: Judul halaman (opsional) --}}
 @section('title', 'Data Label')
-
-{{-- SECTION: Konten utama --}}
 @section('content')
 
     {{-- SECTION: Header halaman --}}
     <div class="container-fluid px-4">
-        <h1 class="mt-4">Dashboard</h1>
-        <ol class="breadcrumb mb-4">
-            <li class="breadcrumb-item active">Dashboard</li>
-        </ol>
+            <h1 class="h3 mb-3 text-gray-800">Label Barang Bukti</h1>
 
         {{-- SECTION: Card tabel data --}}
         <div class="card mb-4">
@@ -43,8 +36,6 @@
                                 <td>{{ $item->keterangan }}</td>
                                 <td>
                                     {{-- Tombol Aksi --}}
-                                    <a href="{{ route('label.create', $item->id) }}"
-                                        class="btn btn-sm btn-secondary">Show</a>
                                     <a href="{{ route('label.edit', $item->id) }}" class="btn btn-sm btn-warning">Edit</a>
 
                                     {{-- Tombol Modal Hapus --}}
@@ -97,12 +88,13 @@
     {{-- END Container --}}
 @endsection
 
-{{-- SECTION: Script tambahan --}}
-@push('scripts')
-    <!-- Page level plugins -->
     <script src="{{ asset('sadmin2/vendor/datatables/jquery.dataTables.min.js') }}"></script>
     <script src="{{ asset('sadmin2/vendor/datatables/dataTables.bootstrap4.min.js') }}"></script>
-
-    <!-- Page level custom scripts -->
     <script src="{{ asset('sadmin2/js/demo/datatables-demo.js') }}"></script>
-@endpush
+    <script src="{{ asset('sadmin2/vendor/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('sadmin2/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('sadmin2/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+    <script src="{{ asset('sadmin2/js/sb-admin-2.min.js') }}"></script>
+    <script src="{{ asset('sadmin2/vendor/chart.js/Chart.min.js') }} "></script>
+    <script src="{{ asset('sadmin2/js/demo/chart-area-demo.js') }}"></script>
+    <script src="{{ asset('sadmin2/js/demo/chart-pie-demo.js') }}"></script>
